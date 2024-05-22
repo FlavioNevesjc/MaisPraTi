57280ce33910e5fb0fd9cac2758adfa2526c9982
FlavioNevesjc <flaviosn@gmail.com>
2024-05-22 15:43:43 -0300

    Atualizacao de aulas modulo 3 e alguns exercicios lista 2 modulo 1

---
+++ Modulo1/JavaScript/exercicios/Lista2/exercicio41.js
@@ -0,0 +1,11 @@
+// Nome: Flávio Soares Neves
+
+// Exercicio 41
+
+const pessoa ={ 
+    nome: 'Flavio',
+    idade: 40
+}
+console.log(pessoa.idade);
+pessoa.email = 'flavio@mail.com.br';
+console.log(pessoa.email);
---
+++ Modulo1/JavaScript/exercicios/Lista2/exercicio42.js
@@ -0,0 +1,15 @@
+// Nome: Flávio Soares Neves
+// Validar se está funcionando
+// Exercicio 42
+function inserirDadosArray(inf){
+    dados.arrays = inf;
+}
+
+const dados = { 
+    numero: 40,
+    string: 'Teste',
+    arrays: []
+}
+
+inserirDadosArray('Dados');
+console.log(dados.arrays);
